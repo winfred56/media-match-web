a861e622a556c6d92b0af2d7a2fd7194b5ef4b57
Gyekye <mrgyekye740@gmail.com>
2024-05-28 11:06:22 +0000

    feat: upload video

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,134 +1,153 @@
-"use client"
-import MediaUpload from "@/app/components/upload_button";
-import Image from "next/image";
-import python from "@/public/python.png"
-import figma from "@/public/figma.png"
-import django from "@/public/django.png"
-import firebase from "@/public/firebase.png"
-import vercel from "@/public/vercel.png"
-import postgresql from "@/public/postgresql.png"
-import flutter from "@/public/flutter.png"
-import tailwind from "@/public/tailwind.png"
-import dart from "@/public/dart.png"
-import github from "@/public/github.png"
-import git from "@/public/git.png"
-import mathplotlib from "@/public/mathplotlib.png"
-import scipy from "@/public/scipy.png"
-import next from "@/public/nextjs.png"
-import numpy from "@/public/numpy.png"
-import typescript from "@/public/typescript.png"
-import {useEffect} from "react";
-
-
-const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+'use client'
+import React, { useRef, useState } from "react";
 
 export default function Header() {
+    const [file, setFile] = useState<File | null>(null);
+    const [loading, setLoading] = useState<boolean>(false);
+    const [error, setError] = useState<string>('');
+    const [progress, setProgress] = useState<number>(0);
+    const [abortController, setAbortController] = useState<AbortController | null>(null);
+    const inputFileRef = useRef<HTMLInputElement>(null);
 
-    const tools= [
-        {"src": flutter},
-        {"src": python},
-        {"src": django},
-        {"src": postgresql},
-        {"src": next},
-        {"src": figma},
-        {"src": firebase},
-        {"src": tailwind},
-        {"src": dart},
-        {"src": scipy},
-        {"src": github},
-        {"src": mathplotlib},
-        {"src": git},
-        {"src": vercel},
-        {"src": numpy},
-        {"src": typescript},
-        {"src": flutter},
-        {"src": python},
-        {"src": django},
-        {"src": postgresql},
-        {"src": next},
-        {"src": figma},
-        {"src": firebase},
-        {"src": tailwind},
-        {"src": dart},
-        {"src": scipy},
-        {"src": github},
-        {"src": mathplotlib},
-        {"src": git},
-        {"src": vercel},
-        {"src": numpy},
-        {"src": typescript},
-
-    ]
-    useEffect(() => {
-    const phrases = [" song.", " video.", " movie."];
-    const el = document.getElementById("typewriter");
-    let sleepTime = 100;
-    let curPhraseIndex = 0;
-
-    const writeLoop = async () => {
-      while (el) {
-        let curWord = phrases[curPhraseIndex];
-
-        for (let i = 0; i < curWord.length; i++) {
-          el.innerText = curWord.substring(0, i + 1);
-          await sleep(sleepTime);
+    const handleFileChange = async (event: React.ChangeEvent<HTMLInputElement>) => {
+        const selectedFile = event.target.files?.[0];
+        console.log(selectedFile);
+        if (selectedFile) {
+            setFile(selectedFile);
+            await handleSubmit(selectedFile);  // Start upload immediately after file is selected
         }
-
-        await sleep(sleepTime * 10);
-
-        for (let i = curWord.length; i > 0; i--) {
-          el.innerText = curWord.substring(0, i - 1);
-          await sleep(sleepTime);
-        }
-
-        await sleep(sleepTime * 5);
-
-        if (curPhraseIndex === phrases.length - 1) {
-          curPhraseIndex = 0;
-        } else {
-          curPhraseIndex++;
-        }
-      }
     };
 
-    writeLoop();
-  }, []);
+    const handleSubmit = async (selectedFile?: File) => {
+        setLoading(true);
+        setProgress(0);
+        const controller = new AbortController();
+        setAbortController(controller);
+
+        if (!selectedFile && !file) {
+            setError('Please select a file.');
+            setLoading(false);
+            return;
+        }
+
+        try {
+            const formData = new FormData();
+            formData.append('file', selectedFile || file as File);
+
+            const xhr = new XMLHttpRequest();
+            xhr.open('POST', 'https://media-match-backend.onrender.com/add/');
+            xhr.upload.onprogress = (event) => {
+                if (event.lengthComputable) {
+                    const percentCompleted = Math.round((event.loaded * 100) / event.total);
+                    setProgress(percentCompleted);
+                }
+            };
+            xhr.onload = () => {
+                if (xhr.status === 200) {
+                    setFile(null);
+                    setError('');
+                    setLoading(false);
+                    window.location.reload();
+                } else {
+                    throw new Error('Failed to upload file.');
+                }
+            };
+            xhr.onerror = () => {
+                setError('Failed to upload file.');
+                setLoading(false);
+            };
+            xhr.onabort = () => {
+                setError('Upload cancelled.');
+                setLoading(false);
+            };
+
+            xhr.send(formData);
+        } catch (error: any) {
+            setError(error.toString());
+            setLoading(false);
+        }
+    };
+
+    const handleCardClick = () => {
+        if (inputFileRef.current) {
+            inputFileRef.current.click();
+        }
+    };
+
+    const handleCancelUpload = () => {
+        if (abortController) {
+            abortController.abort();
+            setAbortController(null);
+            setLoading(false);
+        }
+    };
 
     return (
-        <div
-            className={`flex flex-col text-text_color items-center justify-center w-full mt-0 overflow-hidden xl:h-screen`}>
-            <h1 className={`text-4xl xl:text-8xl text-center font-extrabold pb-6 xl:-mt-20`}>
-                Effortlessly Identify Any <br className={`hidden xl:flex`}/>
-                <span id={`typewriter`}></span>
-                <span id={`typewriter-cursor`}>|</span>
-            </h1>
+        <div className={`flex flex-col justify-center items-center w-full overflow-hidden`}>
+            <div className={`w-[63%] flex flex-row gap-10 items-center justify-center`}>
+                <div className={`h-96`}>
+                    <h2 className={`text-6xl font-extrabold`}>Add Media For Others <br className={`hidden md:flex`} />To
+                        Easily Identify</h2>
 
-            <h5 className={`flex items-center text-center text-lg font-normal relative my-1 xl:w-2/4 px-8`}>
-                We transform your video, song, or movie into a unique digital fingerprint with a distinct hash, enabling
-                seamless recognition and matching for others.
-                {/*<br/>Automatic and 100% free*/}
-            </h5>
-            {/*<button*/}
-            {/*    className={`cta-btn btn`}>*/}
-            {/*    Upload media*/}
-            {/*</button>*/}
-            <MediaUpload />
-
-            <p className={`my-1 text-sm flex text-center items-center justify-center`}>Unlock Instant Recognition with Our Video and Audio Hashing!</p>
-            {/*<MediaUpload/>*/}
-
-            <div className="mt-8 lg:pt-4 flex flex-col items-center justify-center space-y-6 mb-20 xl:mb-0">
-                <div className="container max-w-[800px] flex flex-wrap justify-center items-center">
-                    <div className="carousel-container-clients carousel-fade-clients">
-                        <div className="carousel-clients space-x-7 ">
-                            {tools.map((tool, index) => (
-                                <Image width={500} height={500} className={`w-full h-full invert-effect`} key={index} src={tool.src} alt={`tool logo`}/>
-                            ))}
+                    <h2 className={`flex items-center relative`}>
+                        Automatic and 100% free
+                    </h2>
+                </div>
+                <div className={`w-96 mb-12 mt-60`}>
+                    <div
+                        className={`h-96 p-4 mb-4 drop-shadow-2xl bg-white border-0 rounded-3xl flex flex-col items-center justify-center`}
+                        onClick={handleCardClick}
+                        style={{ cursor: 'pointer' }}
+                    >
+                        <div>
+                            <form onSubmit={(e) => e.preventDefault()}>
+                                <input
+                                    type="file"
+                                    accept="video/*"
+                                    name="file"
+                                    onChange={handleFileChange}
+                                    ref={inputFileRef}
+                                    style={{ display: 'none' }}
+                                />
+                            </form>
+                            {error && <div className={`text-red-500 mt-2`}>Error: {error}</div>}
                         </div>
+                        {loading ? (
+                            <div className={`w-full mt-4`}>
+                                <div className={`relative pt-1`}>
+                                    <div className={`flex mb-2 items-center justify-between`}>
+                                        <div>
+                                            <span className={`text-xs font-semibold inline-block py-1 px-4 uppercase rounded-full text-blue-600 bg-blue-200`}>
+                                                Uploading {progress}%
+                                            </span>
+                                        </div>
+                                    </div>
+                                    <div className={`overflow-hidden h-2 mb-4 text-xs flex rounded bg-blue-200`}>
+                                        <div style={{ width: `${progress}%` }} className={`shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-blue-600`}></div>
+                                    </div>
+                                    <button
+                                        className={`rounded-3xl text-xs bg-red-700 px-4 py-3 font-bold text-white text-center`}
+                                        onClick={handleCancelUpload}
+                                    >
+                                        Cancel Upload
+                                    </button>
+                                </div>
+                            </div>
+                        ) : (
+                            <button
+                                className={`rounded-3xl text-xs bg-blue-700 px-4 py-3 font-bold text-white my-8`}
+                                onClick={() => file && handleSubmit(file)}
+                                disabled={loading}
+                            >
+                                {loading ? 'Uploading...' : 'Upload'}
+                            </button>
+                        )}
+                        <p className={`text-xs text-slate-700`}>You can only upload video and audio files</p>
                     </div>
+                    <p className={`text-sm`}>By uploading an audio or video you agree to our Terms of Service. To learn
+                        more about how we handle your personal data, check our Privacy Policy.</p>
                 </div>
             </div>
         </div>
-
     )
 }
